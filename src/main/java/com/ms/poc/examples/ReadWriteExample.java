5b2c0a5c4b6be51efe6c21b413fca05e72ed38d3
Mauricio O. de Souza <orozimbro@gmail.com>
2019-01-03 19:35:27 -0200

    adding testing on container

--- src/main/java/com/ms/poc/examples/ReadWriteExample.java
+++ src/main/java/com/ms/poc/examples/ReadWriteExample.java
@@ -10,7 +10,7 @@ public class ReadWriteExample {
 	public static void main(String[] args) {
 
         // Syntax: redis://[password@]host[:port][/databaseNumber]
-        RedisClient redisClient = RedisClient.create(RedisURI.create("redis://password@localhost:6379/0"));
+        RedisClient redisClient = RedisClient.create(RedisURI.create("redis://10.2.0.125:6379/0"));
 
         StatefulRedisConnection<String, String> connection = redisClient.connect();
 
--- src/test/java/com/ms/poc/LettucePocMvnApplicationTests.java
+++ src/test/java/com/ms/poc/LettucePocMvnApplicationTests.java
@@ -5,12 +5,32 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LettucePocMvnApplicationTests {
 
 	@Test
 	public void contextLoads() {
+        // Syntax: redis://[password@]host[:port][/databaseNumber]
+        RedisClient redisClient = RedisClient.create(RedisURI.create("redis://localhost:6379/0"));
+
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+
+        System.out.println("Connected to Redis");
+
+        RedisCommands<String, String> sync = connection.sync();
+
+        sync.set("foo", "bar");
+        String value = sync.get("foo");
+        System.out.println(value);
+
+        connection.close();
+        redisClient.shutdown();
 	}
 
 }
